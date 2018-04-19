ef79aabb04c52b5c351a827c29da7cf65435489d
xxyy <xxyyxyyxxyyx@gmail.com>
2018-04-19 11:58:20 +0800

    commit

--- src/main/resources/mysql/data.sql
+++ src/main/resources/mysql/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (username, password, role, department)
-VALUES ('user', '$2a$10$9/44Rne7kQqPXa0cY6NfG.3XzScMrCxFYjapoLq/wFmHz7EC9praK', 'ROLE_USER', 'cosmetic', 'xxx');
+VALUES ('user', '$2a$10$9/44Rne7kQqPXa0cY6NfG.3XzScMrCxFYjapoLq/wFmHz7EC9praK', 'ROLE_USER', 'cosmetic');
 INSERT INTO users (username, password, role, department)
 VALUES ('admin', '$2a$10$9/44Rne7kQqPXa0cY6NfG.3XzScMrCxFYjapoLq/wFmHz7EC9praK', 'ROLE_ADMIN', 'it');
